6741acdc340128a9adad37dd223fcb4a16783afe
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-05-12 16:12:09 +0600

    require body-parser , uuid , express and port 5000

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const dialogflow = require("@google-cloud/dialogflow");
 const uuid = require("uuid");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 5000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 /**
  * Send a query to the dialogflow agent, and return the query result.
