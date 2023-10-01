9846deb970c8559b2273914c3dca8364f435be89
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-10-01 11:27:01 +0600

    Number of trained training phrases

---
+++ backend/NoOfTrainedPhrases.py
@@ -0,0 +1,30 @@
+import requests
+
+# Set up your Dialogflow project ID and access token
+project_id = "test-bot-jacm"
+project_id2 = "palette-chat-qljh"
+access_token = ""
+
+# Set up the API endpoint for listing intents
+api_endpoint = (
+    f"https://dialogflow.googleapis.com/v2/projects/{project_id2}/agent/intents"
+)
+
+# Set the request headers with the access token
+headers = {
+    "Authorization": f"Bearer {access_token}",
+    "Content-Type": "application/json",
+}
+
+# Make the API request to list intents
+response = requests.get(api_endpoint, headers=headers)
+
+# Check the status code to ensure the request was successful
+if response.status_code == 200:
+    intents = response.json().get("intents", [])
+    total_training_phrases = sum(
+        len(intent.get("trainingPhrases", [])) for intent in intents
+    )
+    print(f"Number of trained training phrases: {total_training_phrases}")
+else:
+    print("Error retrieving intents")
